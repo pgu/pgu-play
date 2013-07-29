87c127d6c7c7e43acd72acd0ae0b9380903edcf6
Pascal Guilcher <guilcher.p@sfeir.com>
2013-07-29 15:45:48 +0200

    add FireFox Os specs

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,9 +2,31 @@
 
 angular.module('pguPlayApp')
   .controller('MainCtrl', function ($scope) {
+
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.isAppAlreadyInstall = false;
+
+    var urlOfManifest = location.hostname.indexOf('localhost') !== -1 ? "http://localhost:9000/manifest.webapp" : "http://pgu-play.appspot.com/manifest.webapp";
+
+    var installCheck = navigator.mozApps.checkInstalled(urlOfManifest);
+    installCheck.onsuccess = function() {
+        $scope.isAppAlreadyInstall = _.isObject(installCheck.result);
+    };
+
+    $scope.installOnFirefoxOs = function() {
+        // install the app
+        var installAppli = navigator.mozApps.install(urlOfManifest);
+        installAppli.onsuccess = function(data) { // App is installed
+            $scope.isAppAlreadyInstall = true;
+        };
+        installAppli.onerror = function() { // App wasn't installed, info is in installapp.error.name
+            console.log(installapp.error.name);
+        };
+    }
+
   });
