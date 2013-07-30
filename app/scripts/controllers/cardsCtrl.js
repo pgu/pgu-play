72cba20cc0a0d1fed233a01a44e34378b45f5cce
Pascal Guilcher <guilcher.p@sfeir.com>
2013-07-30 15:37:06 +0200

    ..

--- app/scripts/controllers/cardsCtrl.js
+++ app/scripts/controllers/cardsCtrl.js
@@ -1,9 +1,8 @@
 'use strict';
 
 angular.module('pguPlayApp').controller('CardsCtrl', //
-    [ '$scope', 'LanguagesSrv', //
-        function ($scope, LanguagesSrv) { //
-
+    [ '$scope', 'LanguagesSrv', '$timeout', //
+        function ($scope, LanguagesSrv, $timeout) { //
 
             $scope.namesOfLg = [];
             $scope.selectedLg = undefined;
@@ -12,6 +11,7 @@ angular.module('pguPlayApp').controller('CardsCtrl', //
 
             var itemToGuess = undefined;
             var itemsOfGame = [];
+            var itemsOfGameSource = [];
 
             var getNamesOfLanguages = function() {
                 return _.map(LanguagesSrv.languages, function (v, k, list) {
@@ -29,7 +29,8 @@ angular.module('pguPlayApp').controller('CardsCtrl', //
 
                 var selectedLg = _.findWhere(LanguagesSrv.languages, {name: nameOfLg});
 
-                itemsOfGame = _.clone(selectedLg.data);
+                itemsOfGameSource = _.clone([selectedLg.data[0], selectedLg.data[1], selectedLg.data[2]]);
+                itemsOfGame = _.clone(itemsOfGameSource);
 
                 $scope.selectedLg = selectedLg;
 
@@ -40,12 +41,33 @@ angular.module('pguPlayApp').controller('CardsCtrl', //
                 answer.state = answer.value === itemToGuess[1] ? 'success' : 'error';
 
                 if (answer.state === 'success') {
-                    playGame();
+                    $timeout(function () {
+                        playGame();
+                    }, 300);
                 }
 
             };
 
             var playGame = function() {
+
+                console.log(itemsOfGame);
+
+                var gameIsOver = itemsOfGame.length === 0;
+                if (gameIsOver) {
+
+                    $scope.selectedLg = undefined;
+                    $scope.selectedItem = undefined;
+                    $scope.itemToGuessDisplay = undefined;
+
+                    itemToGuess = undefined;
+                    itemsOfGame = [];
+
+                    return;
+                }
+
+                //
+                // Play the game!
+                //
 
                 // select to symbol to guess
                 var selectedIdx = getRandomInt(0, itemsOfGame.length);
@@ -54,8 +76,9 @@ angular.module('pguPlayApp').controller('CardsCtrl', //
                 itemsOfGame.splice(selectedIdx, 1); // clean the game for the next round
 
                 // select wrong answers
-                var itemsForWrongAnswers = _.clone(itemsOfGame);
-                itemsForWrongAnswers.splice(selectedIdx, 1);
+                var itemsForWrongAnswers = _.filter(itemsOfGameSource, function(item) {
+                    return item[0] !== selectedItem[0] || item[1] !== selectedItem[1];
+                });
 
                 var wrongItem1Idx = getRandomInt(0, itemsForWrongAnswers.length);
                 var wrongItem1 = itemsForWrongAnswers[wrongItem1Idx];
@@ -63,6 +86,7 @@ angular.module('pguPlayApp').controller('CardsCtrl', //
                 itemsForWrongAnswers.splice(wrongItem1Idx, 1);
                 var wrongItem2 = itemsForWrongAnswers[getRandomInt(0, itemsForWrongAnswers.length)];
 
+                // build answers
                 var sortedItems = [selectedItem, wrongItem1, wrongItem2];
                 var sortedAnswers = _.map(sortedItems, function(item) {
                     return item[1];
@@ -72,15 +96,16 @@ angular.module('pguPlayApp').controller('CardsCtrl', //
                 for (var i = 0; i < sortedItems.length; i++) {
                     var idxToPush = getRandomInt(0, sortedAnswers.length);
 
-                    var answerUI = {
+                    var answerForView = {
                         value: sortedAnswers[idxToPush],
                         state: 'clean'
                     };
-                    randomAnswers.push(answerUI);
+                    randomAnswers.push(answerForView);
 
                     sortedAnswers.splice(idxToPush, 1);
                 }
 
+                //
                 itemToGuess = selectedItem;
 
                 $scope.answers = randomAnswers;
