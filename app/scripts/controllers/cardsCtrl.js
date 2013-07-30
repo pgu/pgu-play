46b96393e72b52a9a377ef956f36d950f40ed20c
Pascal Guilcher <guilcher.p@sfeir.com>
2013-07-30 12:12:49 +0200

    ..

--- app/scripts/controllers/cardsCtrl.js
+++ app/scripts/controllers/cardsCtrl.js
@@ -1,9 +1,17 @@
 'use strict';
 
-angular.module('pguPlayApp').controller('CardsCtrl',
-    [ '$scope', 'LanguagesSrv',
-        function ($scope, LanguagesSrv) {
+angular.module('pguPlayApp').controller('CardsCtrl', //
+    [ '$scope', 'LanguagesSrv', //
+        function ($scope, LanguagesSrv) { //
 
-    $scope.name = 'cards';
+            $scope.selectedLg = undefined;
 
-}]);
+            $scope.namesOfLg = _.map(LanguagesSrv.languages, function (v, k, list) {
+                return v.name;
+            });
+
+            $scope.selectLanguage = function (nameOfLg) {
+                var selectedLg = _.findWhere(LanguagesSrv.languages, {name: nameOfLg});
+                $scope.selectedLg = selectedLg;
+            }
+        }]);
