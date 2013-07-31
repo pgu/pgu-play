36e7336366c9b417be12517591909502d0b321f2
Pascal Guilcher <guilcher.p@sfeir.com>
2013-07-31 17:26:35 +0200

    add glyphicons from bootstrap

--- app/scripts/directives/homeBtnModule.js
+++ app/scripts/directives/homeBtnModule.js
@@ -5,7 +5,7 @@ angular.module('homeBtnModule', []);
 angular.module('homeBtnModule').directive('homeBtn', function() {
     return {
         restrict: 'E',
-        template: '<a type="button" class="btn btn-large btn-primary" href="/">Home</a>',
+        template: '<a type="button" class="btn btn-large btn-primary glyphicon glyphicon-home" href="/"></a>',
         replace: true
     };
 });
