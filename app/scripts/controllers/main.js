22420a77c79e19d6ff561aa359f551fa841f9c22
Pascal Guilcher <guilcher.p@sfeir.com>
2013-07-31 17:29:00 +0200

    ..

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,45 +4,25 @@ angular.module('pguPlayApp').controller('MainCtrl',
     [ '$scope',
         function ($scope) {
 
-    $scope.awesomeThings = [
-        'HTML5 Boilerplate', 'AngularJS', 'Karma'
-    ];
-
     $scope.isInFF = _.isObject(navigator.mozApps);
 
     if ($scope.isInFF) {
 
-        $scope.isAppInstalled = false;
-        $scope.infos = ['init'];
-
         var host = location.hostname.indexOf('localhost') !== -1 ? 'http://localhost:9000' : 'http://pgu-play.appspot.com';
         var urlOfManifest = host + '/manifest.webapp';
-        $scope.infos.push('url of manifest: ' + urlOfManifest);
 
         var installCheck = navigator.mozApps.checkInstalled(urlOfManifest);
         installCheck.onsuccess = function () {
-            $scope.isAppInstalled = _.isObject(installCheck.result);
-            $scope.infos.push('installCheck.onsuccess: ' + installCheck.result);
-
         };
         installCheck.onerror = function () {
-            $scope.isAppInstalled = false;
-            $scope.infos.push('Error calling checkInstalled: ' + this.error.name);
         };
 
         $scope.installOnFirefoxOs = function () {
-            $scope.infos.push('installOnFF ');
 
             var installApp = navigator.mozApps.install(urlOfManifest);
             installApp.onsuccess = function () { // App is installed
-                $scope.isAppInstalled = true;
-                $scope.infos.push('installOnFF.onsuccess ');
-                $scope.infos.push(this.result);
             };
             installApp.onerror = function () { // App wasn't installed, info is in installapp.error.name
-                $scope.isAppInstalled = false;
-                $scope.infos.push('installOnFF.onerror ');
-                $scope.infos.push(this.error.name);
             };
         };
     }
