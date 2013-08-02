f7d81d9c53025d9ac83cfa0a34b6ebf0e653a2b5
Pascal Guilcher <guilcher.p@sfeir.com>
2013-08-02 17:28:40 +0200

    add korean

--- app/scripts/services/languagesSrv.js
+++ app/scripts/services/languagesSrv.js
@@ -85,6 +85,18 @@ angular.module('pguPlayApp').factory('LanguagesSrv', ['KanjiSrv', function (Kanj
       ['ﻻ', 'la'], ['ﻼ', 'la'] //
     ]);
 
+    var korean = Object.freeze([ //
+        ['ㄱ',  'g'], ['ㄴ',   'n'], ['ㄷ',   'd'], ['ㄹ',   'l'], ['ㅁ',  'm'], //
+        ['ㅂ',  'b'], ['ㅅ',   's'], ['ㅇ',  'ng'], ['ㅈ',   'j'], ['ㅊ', 'ch'], //
+        ['ㅋ',  'k'], ['ㅌ',   't'], ['ㅍ',   'p'], ['ㅎ',   'h'], //
+        ['ㄲ', 'kk'], ['ㄸ',  'tt'], ['ㅃ',  'pp'], ['ㅆ',  'ss'], ['ㅉ', 'jj'], //
+        ['ㅏ',  'a'], ['ㅑ',  'ya'], ['ㅓ',  'eo'], ['ㅕ', 'yeo'], //
+        ['ㅗ',  'o'], ['ㅛ',  'yo'], ['ㅜ',   'u'], ['ㅠ',  'yu'], ['ㅡ', 'eu'], //
+        ['ㅣ',  'i'], ['ㅐ',  'ae'], ['ㅒ', 'yae'], ['ㅔ',   'e'], ['ㅖ', 'ye'], //
+        ['ㅘ', 'wa'], ['ㅙ', 'wae'], ['ㅚ',  'oe'], ['ㅝ',  'wo'], ['ㅞ', 'we'], //
+        ['ㅟ', 'wi'], ['ㅢ',  'ui'] //
+    ]);
+
     return {
         languages: [
             {
@@ -116,6 +128,11 @@ angular.module('pguPlayApp').factory('LanguagesSrv', ['KanjiSrv', function (Kanj
                 key: 'arabicLong',
                 name: 'Arabic [+]',
                 data: arabicLong
+            },
+            {
+                key: 'korean',
+                name: 'Korean',
+                data: korean
             },
             {
                 key: 'kanji',
