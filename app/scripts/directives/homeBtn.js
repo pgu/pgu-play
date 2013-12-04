6a6547b9fc6bcb1c34615136693a388bf05f50cd
Pascal Guilcher <guilcher.p@sfeir.com>
2013-12-04 11:19:57 +0100

    update glyphicons after updating bootstrap to 3.0.2

--- app/scripts/directives/homeBtn.js
+++ app/scripts/directives/homeBtn.js
@@ -4,7 +4,7 @@ angular.module('pguPlayApp').directive('homeBtn', function() {
     return {
         restrict: 'E',
         replace: true,
-        template: '<p class="text-right"><a type="button" class="btn btn-lg btn-primary glyphicon glyphicon-home" href="/" ng-click="onGoHome()"></a></p>',
+        template: '<p class="text-right"><a type="button" class="btn btn-lg btn-primary" href="/" ng-click="onGoHome()"><span class="glyphicon glyphicon-home"></span></a></p>',
         scope: {
             onGoHome: '&'
         }
